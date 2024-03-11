0c549330be51ea38df8e28974f38240f6fc51400
bobbyhao <bobbyhao0011@163.com>
2024-03-11 18:33:18 +0800

    bobbyhao<bobbyhao0011@163.com>

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -17,5 +17,7 @@ public class Test1 {
         System.out.println("test4");
         System.out.println("test5");
         System.out.println("test6");
+
+        System.out.println("test7");
     }
 }
